362efdc9ffe86628378c173804a3709028d43a1f
turco-vic <enzoturcovic@gmail.com>
2026-03-31 22:08:57 -0300

    chore: add Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import theme from "./theme";
 import "./i18n";
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <ChakraProvider theme={theme}>
         <App />
+        <Analytics />
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
